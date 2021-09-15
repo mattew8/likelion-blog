e7ddc77d7ea37b4dd2cc168216529134916a3bee
mattew8 <mattew4483@likelion.org>
2021-09-15 18:23:56 +0900

    [add] model 생성 및 list페이지 생성

--- likelion/blog/models.py
+++ likelion/blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=20)  # 글자 수 제한 O
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(
+        "Category", on_delete=CASCADE, related_name="post")
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    subject = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.subject  # admin에서 카테고리의 subject를 보여줄꺼야
+
+# 댓글 -> 하나의 글은 ; 여러개의 댓글 O
+# 하나의 카테고리 : 여러개의 Post O
+
+# 1. 데이터 구조변화 기록 -> makemigrations
+# 2. 해당 기록들을 실재로 DB에 적용한다 -> migrate
 
 # Create your models here.
--- likelion/blog/views.py
+++ likelion/blog/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from .models import Post, Category
+
 
 def index(request):
-    return render(request, "index.html")
+    # 모든 카테고리를 인덱스 페이지에 뿌려준다
+    categories = Category.objects.all()
+
+    context = {"categories": categories}
+
+    return render(request, "index.html", context)
+
+
+def list(request, item_id):
+    selected_category = Category.objects.get(pk=item_id)
+    # 카테고리 데이터에서 pk가 item_id인 카테고리 하나만 가져왔다
+    posts = selected_category.post.all()
+    # 선택된 카테고리에 해당하는 posts들을 전부다 가져왔다
+
+    context = {"selected_category": selected_category, "posts": posts}
+
+    return render(request, "list.html", context)
+
 
 # Create your views here.
